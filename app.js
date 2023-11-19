5aa0c9c6a241ee748174e96179a8890d2d7518b7
Ha Huy Hung <hahuyhungdev@gmail.com>
2023-11-19 17:48:41 +0700

    update handle sound

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ var boxgift = document.querySelector(".box-gift");
 var Close = document.querySelector(".fa-xmark");
 var boxContent = document.querySelector(".box-content");
 var content = document.querySelector(".content");
-window.onload = function () {
-  var audio = document.getElementById("backgroundMusic");
-  audio.play();
-};
+// window.onload = function () {
+//   var audio = document.getElementById("backgroundMusic");
+//   audio.play();
+// };
+var audio = document.getElementById("backgroundMusic");
+function playAudio() {
+  audio
+    .play()
+    .then(() => {
+      // Remove the event listener after the audio starts playing
+      document.removeEventListener("click", playAudio);
+    })
+    .catch((error) => {
+      console.log("Audio play failed:", error);
+    });
+}
+
+// Attach the event listener to the whole document
+document.addEventListener("click", playAudio);
 boxgift.onclick = function () {
   boxgift.classList.toggle("active");
   // boxContent.classList.add('active')
