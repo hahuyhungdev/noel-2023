8f12e2d8935f0d8a59a42ee753bb1b23c8d22655
Ha Huy Hung [Huy.Hung] <huy.hung@nexondv.com>
2023-12-21 16:25:34 +0700

    feat: implement prj

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-var boxgift = document.querySelector(".box-gift");
-var Close = document.querySelector(".fa-xmark");
-var boxContent = document.querySelector(".box-content");
-var content = document.querySelector(".content");
-// window.onload = function () {
-//   var audio = document.getElementById("backgroundMusic");
-//   audio.play();
-// };
 var audio = document.getElementById("backgroundMusic");
 function playAudio() {
   audio
@@ -18,16 +10,3 @@ function playAudio() {
       console.log("Audio play failed:", error);
     });
 }
-
-// Attach the event listener to the whole document
-document.addEventListener("click", playAudio);
-boxgift.onclick = function () {
-  boxgift.classList.toggle("active");
-  // boxContent.classList.add('active')
-};
-content.onclick = function () {
-  boxContent.classList.add("active");
-};
-Close.onclick = function () {
-  boxContent.classList.remove("active");
-};
